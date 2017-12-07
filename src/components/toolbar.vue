7cfae45ed0b54d992268df84b5494205b35c7c1a
Artila <529276960@qq.com>
2017-12-07 22:16:26 +0800

    侧边栏

---
+++ src/components/toolbar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="toolbar">
+    <i class="glyphicon glyphicon-plus"></i>
+    <i class="glyphicon glyphicon-star"></i>
+    <i class="glyphicon glyphicon-remove"></i>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  #toolbar {
+    float: left;
+    padding: 35px 25px 25px 25px;
+    width: 80px;
+    height: 100%;
+    background-color: #30414d;
+    color: #767676;
+  }
+
+  #toolbar i {
+    margin-bottom: 35px;
+    font-size: 30px;
+    opacity: 0.8;
+    cursor: pointer;
+    transition: opacity 0.5s ease;
+  }
+
+  #toolbar i:hover {
+    opacity: 1;
+  }
+
+  .star {
+    color: #f7ae4f;
+  }
+</style>
