562b02f25a3463c06435ecb68d92bfa80b286335
Artila <529276960@qq.com>
2017-12-07 22:16:47 +0800

    编辑区

---
+++ src/components/editor.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="editor">
+    <textarea></textarea>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+  #editor {
+    margin-left: 380px;
+    height: 100%;
+  }
+
+  textarea {
+    height: 100%;
+    border: 0;
+    border-radius: 0;
+  }
+</style>
