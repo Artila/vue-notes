5d76e646976d5c7b4dbb01f5f1402ec2c1201c51
Artila <529276960@qq.com>
2017-12-07 22:17:00 +0800

    首页

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <toolbar></toolbar>
+    <note-list></note-list>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+  import Editor from './components/editor'
+  import NoteList from './components/notelist'
+  import Toolbar from './components/toolbar'
+
+  export default {
+    components: {
+      Editor,
+      NoteList,
+      Toolbar
+    }
+  }
+</script>
+
+<style>
+  html, #app {
+    height: 100%;
+  }
+
+  body {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    height: 100%;
+    max-height: 100%;
+  }
+</style>
