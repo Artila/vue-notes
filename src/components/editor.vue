0abf9a7f3dabd9cb1a3f3e95c48751455d3ab263
Artila <529276960@qq.com>
2017-12-12 23:01:32 +0800

    编辑区

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,19 +1,57 @@
 <template>
   <div id="editor">
-    <textarea></textarea>
+  	<div class="form-group">
+  		<!--<input 
+  			v-model="title"
+  			type="text" name="title" 
+  			class="title form-control" 
+  			placeholder="请输入标题">-->
+  		<textarea 
+  			:value="activeNoteContent"
+  			@input="editNote"
+  			name="content" class="form-control" 
+  			placeholder="请输入正文"></textarea>
+  	</div>  
   </div>
 </template>
 
 <script>
+	import { mapGetters, mapActions } from 'vuex';
+	
+	export default {
+		computed: {
+			...mapGetters([
+				'activeNoteContent'
+			])
+		},
+		methods: {
+			...mapActions([
+				'editNote'
+			])
+		}
+	}
 </script>
 
 <style>
   #editor {
-    margin-left: 380px;
-    height: 100%;
+    flex: 2;
+  }
+  
+  .form-group {
+  	display: flex;
+  	flex-flow: column;
+  	height: 100%;
+  	margin-bottom: 0;
+  }
+  
+  .title {
+  	border: 0;
   }
 
-  textarea {
+  #editor textarea {
+  	flex-grow: 2;
+  	box-sizing: border-box;
+  	padding-top: 15px;
     height: 100%;
     border: 0;
     border-radius: 0;
